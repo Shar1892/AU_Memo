3b2966aa719ff50a850b6a125d5cf8fd73f1dd72
sg.sharov <sg.sharov@tensor.ru>
2022-08-04 19:22:01 +0300

    feat: сделал компонент цитаты из серии

---
+++ src/components/seriesQuote/seriesQuote.js
@@ -0,0 +1,20 @@
+import './seriesQuote.css';
+
+function SeriesQuote({data}) {
+	return (
+		<div className='seriesQuote'>
+			<p className='seriesQuote__title'>Цитата</p>
+			<p className='seriesQuote__text'>{data.text}</p>
+			<div className='seriesQuote__person-container'>
+				<img
+					className='seriesQuote__person-photo'
+					src={data.autorsPhoto}
+					alt={data.autor}
+				></img>
+				<p className='seriesQuote__person'>{data.autor}</p>
+			</div>
+		</div>
+	);
+}
+
+export default SeriesQuote;
