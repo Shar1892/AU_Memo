14637a0751b838bffdbb0bab5dffaddc32e2adb8
sg.sharov <sg.sharov@tensor.ru>
2022-08-09 13:14:01 +0300

    feat: подключил шрифты

--- src/components/seriesText/seriesText.js
+++ src/components/seriesText/seriesText.js
@@ -3,7 +3,6 @@ import './seriesText.css';
 function SeriesText({data}) {
 	return (
 		<div className='seriesText'>
-			<audio src={data.recordSrc} type='audio/mp3' loop controls></audio>
 			<p className='seriesText__content'>{data.content}</p>
 		</div>
 	);
