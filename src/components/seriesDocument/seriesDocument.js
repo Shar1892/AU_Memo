61df91811a4caccf07865d67c314836b1a4eb0ca
sg.sharov <sg.sharov@tensor.ru>
2022-08-10 20:19:09 +0300

    feat: добавил сворачивание/разворачивание документа

--- src/components/seriesDocument/seriesDocument.js
+++ src/components/seriesDocument/seriesDocument.js
@@ -1,11 +1,41 @@
+import {useState} from 'react';
+
 import './seriesDocument.css';
 
 function SeriesDocument({data}) {
+	const [isShort, setIsShort] = useState(true);
+
+	const expandDocument = () => {
+		setIsShort(!isShort);
+	};
+
 	return (
 		<div className='seriesDocument'>
 			<p className='seriesDocument__title'>Документ</p>
 			<p className='seriesDocument__name'>{data.name}</p>
-			<p className='seriesDocument__text'>{data.text}</p>
+			<p
+				className={`seriesDocument__text ${
+					isShort ? 'seriesDocument__text_short' : ''
+				}`}
+			>
+				{data.text}
+			</p>
+			<button
+				className={`seriesDocument__button ${
+					isShort ? '' : 'seriesDocument__button_hidden'
+				}`}
+				onClick={expandDocument}
+			>
+				Читать полностью
+			</button>
+			<button
+				className={`seriesDocument__button ${
+					isShort ? 'seriesDocument__button_hidden' : ''
+				}`}
+				onClick={expandDocument}
+			>
+				Свернуть
+			</button>
 		</div>
 	);
 }
