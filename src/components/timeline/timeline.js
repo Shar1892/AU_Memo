c1c53f68407ce3a31a545a9c76dba93cb683c449
sg.sharov <sg.sharov@tensor.ru>
2022-08-19 22:46:18 +0300

    feat: сделал шапку таймлайна

--- src/components/timeline/timeline.js
+++ src/components/timeline/timeline.js
@@ -1,6 +1,9 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import {useState} from 'react';
 
 import './timeline.css';
+
+import headerImage from '../../image/timelineHeaderImage.jpg';
 
 import timelineArr from '../../data/timeline';
 import VideoPopup from '../videoPopup/videoPopup';
@@ -38,26 +41,40 @@ function Timeline() {
 	return (
 		<>
 			<main className='timeline'>
-				<h2 className='timeline__title'> Название таймлайна</h2>
-				<div className='timeline__list'>
-					{timelineArr.map((timeline) => (
-						<div className='timeline__part' key={timeline.number}>
-							<p className='timeline__part-date'>{timeline.date}</p>
-							<div className='timeline__graphic'>
-								<div className='timeline__graphic-head'></div>
-								<div className='timeline__graphic-line'></div>
+				<div className='timeline__header'>
+					<p className='timeline__pretitle'>Спецпроект</p>
+					<h2 className='timeline__title'>Мемориал. Ликвидация</h2>
+					<img
+						className='timeline__header-image'
+						src={headerImage}
+						alt='header image'
+					></img>
+					<p className='timeline__subtitle'>
+						Как за 10 лет уничтожили старейшую правозащитную организацию России
+						«Международный Мемориал»
+					</p>
+				</div>
+				<div className='timeline__list-container'>
+					<div className='timeline__list'>
+						{timelineArr.map((timeline) => (
+							<div className='timeline__part' key={timeline.number}>
+								<p className='timeline__part-date'>{timeline.date}</p>
+								<div className='timeline__graphic'>
+									<div className='timeline__graphic-head'></div>
+									<div className='timeline__graphic-line'></div>
+								</div>
+								<div
+									className='timeline__content'
+									onClick={() =>
+										openVideo(timeline.seriesNumber, timeline.startSecond)
+									}
+								>
+									<h3 className='timeline__subtitle'>{timeline.name}</h3>
+									<p className='timeline__text'>{timeline.discription}</p>
+								</div>
 							</div>
-							<div
-								className='timeline__content'
-								onClick={() =>
-									openVideo(timeline.seriesNumber, timeline.startSecond)
-								}
-							>
-								<h3 className='timeline__subtitle'>{timeline.name}</h3>
-								<p className='timeline__text'>{timeline.discription}</p>
-							</div>
-						</div>
-					))}
+						))}
+					</div>
 				</div>
 				<div className='timeline__autors-list'>
 					<p className='timeline__autor'>Автор/ы</p>
