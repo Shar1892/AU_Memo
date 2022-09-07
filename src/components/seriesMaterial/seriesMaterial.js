96280dec7349035998ed984f4d2bbb1b0b9034b7
sg.sharov <sg.sharov@tensor.ru>
2022-09-08 02:20:17 +0300

    refactor: вывод списка материалов вынес в отдельный компонент

---
+++ src/components/seriesMaterial/seriesMaterial.js
@@ -0,0 +1,29 @@
+import './seriesMaterial.css';
+
+import SeriesReference from '../seriesReference/seriesReference';
+import SeriesIllustration from '../seriesIllustration/seriesIllustration';
+import SeriesSketch from '../seriesSketch/seriesSketch';
+import SeriesRecord from '../seriesRecord/seriesRecord';
+
+function SeriesMaterial({materials, isWide}) {
+	return (
+		<div className={`seriesMaterial ${isWide ? 'seriesMaterial_single' : ''}`}>
+			{materials &&
+				materials.map((material, i) => (
+					<div className={`${isWide ? '' : 'seriesMaterial__content'}`} key={i}>
+						{material.type === 'reference' ? (
+							<SeriesReference data={material} />
+						) : material.type === 'record' ? (
+							<SeriesRecord data={material} />
+						) : material.type === 'illustration' ? (
+							<SeriesIllustration data={material} />
+						) : (
+							<SeriesSketch data={material} />
+						)}
+					</div>
+				))}
+		</div>
+	);
+}
+
+export default SeriesMaterial;
