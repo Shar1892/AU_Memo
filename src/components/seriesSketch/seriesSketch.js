6edd9e889cc0b0a35baa0934834dc770cbf0d262
sg.sharov <sg.sharov@tensor.ru>
2023-01-26 02:16:17 +0300

    fix: доработал компонент скетч, на случай отсутствия картинки

--- src/components/seriesSketch/seriesSketch.js
+++ src/components/seriesSketch/seriesSketch.js
@@ -3,7 +3,13 @@ import './seriesSketch.css';
 function SeriesSketch({data}) {
 	return (
 		<div className='seriesSketch'>
-			<img className='seriesSketch__image' src={data.src} alt={data.type}></img>
+			{data.src && (
+				<img
+					className='seriesSketch__image'
+					src={data.src}
+					alt={data.type}
+				></img>
+			)}
 			<p className='seriesSketch__title'>зарисовка</p>
 			<p className='seriesSketch__discription'>{data.discription}</p>
 		</div>
