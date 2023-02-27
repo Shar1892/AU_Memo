038fb07bd54c1d7c0a824104a6a7870aa866b101
sg.sharov <sg.sharov@tensor.ru>
2023-02-28 01:47:33 +0300

    feat: сделал превью для видео серий

--- src/components/seriesVideo/seriesVideo.js
+++ src/components/seriesVideo/seriesVideo.js
@@ -1,17 +1,35 @@
+import {useState} from 'react';
+
 import './seriesVideo.css';
 
 function SeriesVideo({data}) {
+	const [isPreview, setIsPreview] = useState(true);
+
+	const hendleStartVideo = () => {
+		setIsPreview(false);
+	};
+
 	return (
 		<div className='seriesVideo'>
 			<iframe
 				id='ytplayer'
 				type='text/html'
-				src={data.videoSrc}
+				src={data.videoSrc + '?autoplay=1'}
 				frameBorder='0'
 				allowFullScreen
-				className='seriesVideo__video'
+				className={`seriesVideo__video ${
+					isPreview ? 'seriesVideo__video_invisible' : ''
+				}`}
 				title={data.name}
 			></iframe>
+			<img
+				className={`seriesVideo__preview ${
+					isPreview ? '' : 'seriesVideo__preview_invisible'
+				}`}
+				src={data.preview}
+				alt={data.name}
+				onClick={hendleStartVideo}
+			></img>
 			<p className='seriesVideo__name'>{data.name}</p>
 			{data.autor && (
 				<p className='seriesVideo__autor'>{`${data.autor}, ${data.date}`}</p>
