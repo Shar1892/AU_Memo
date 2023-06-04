f225e2648b1fc4d1724c20f4b9b641ad450943e6
sg.sharov <sg.sharov@tensor.ru>
2023-06-05 01:18:25 +0300

    Updates

---
+++ precache-manifest.7c76db56200292ac660d1be637bba436.js
@@ -0,0 +1,554 @@
+self.__precacheManifest = [
+  {
+    "revision": "feeafa09466c95d77b6d",
+    "url": "/AU_Memo/static/css/main.d9ebb79f.chunk.css"
+  },
+  {
+    "revision": "feeafa09466c95d77b6d",
+    "url": "/AU_Memo/static/js/main.feeafa09.chunk.js"
+  },
+  {
+    "revision": "8ac703b7e6f4372fecc6",
+    "url": "/AU_Memo/static/js/1.8ac703b7.chunk.js"
+  },
+  {
+    "revision": "86fa0dca294c25dfa28c",
+    "url": "/AU_Memo/static/js/2.86fa0dca.chunk.js"
+  },
+  {
+    "revision": "374861d5fb23976263c3",
+    "url": "/AU_Memo/static/js/runtime~main.374861d5.js"
+  },
+  {
+    "revision": "30c6977e6660a010b29d670b639bc74b",
+    "url": "/AU_Memo/static/media/Noize_MC.30c6977e.mp3"
+  },
+  {
+    "revision": "e766ec68624baf00df796309c7360f80",
+    "url": "/AU_Memo/static/media/logoMemo.e766ec68.png"
+  },
+  {
+    "revision": "7e98528dc7dbd38e040d20be03c40945",
+    "url": "/AU_Memo/static/media/headerImage1.7e98528d.png"
+  },
+  {
+    "revision": "098dd2849e018d08e397f8dce16a61d9",
+    "url": "/AU_Memo/static/media/headerImage2.098dd284.png"
+  },
+  {
+    "revision": "6d243e6fe31d56472a17ab058f924823",
+    "url": "/AU_Memo/static/media/headerImage3.6d243e6f.png"
+  },
+  {
+    "revision": "502f47a621ae1d4390a42c74ab69dadc",
+    "url": "/AU_Memo/static/media/headerImage4.502f47a6.png"
+  },
+  {
+    "revision": "c2f6e13a84003470afea15771c18dba0",
+    "url": "/AU_Memo/static/media/headerImage5.c2f6e13a.png"
+  },
+  {
+    "revision": "d768ac534104e610ab438b733f643455",
+    "url": "/AU_Memo/static/media/headerImage6.d768ac53.png"
+  },
+  {
+    "revision": "102bc1356481dc2a323a0e147e9f201a",
+    "url": "/AU_Memo/static/media/headerImage7.102bc135.png"
+  },
+  {
+    "revision": "0bc5bfe0fb45422ae2712ee5f328ee60",
+    "url": "/AU_Memo/static/media/mainImage.0bc5bfe0.png"
+  },
+  {
+    "revision": "83b177c5cae0bdc79cfd388a48ee0406",
+    "url": "/AU_Memo/static/media/previewImage.83b177c5.png"
+  },
+  {
+    "revision": "cc759164f47168ef29a050c210f8bfdf",
+    "url": "/AU_Memo/static/media/partHeaderImage1.cc759164.png"
+  },
+  {
+    "revision": "5642cbef9084ef62a750e79b63bd16c6",
+    "url": "/AU_Memo/static/media/partHeaderImage2.5642cbef.png"
+  },
+  {
+    "revision": "a35d2b2f6af8289061face2a632c4860",
+    "url": "/AU_Memo/static/media/illustration1.a35d2b2f.png"
+  },
+  {
+    "revision": "563219ba776c8978875294adc3a4f59f",
+    "url": "/AU_Memo/static/media/illustration2.563219ba.png"
+  },
+  {
+    "revision": "814bba8be79e6e7936698c8502934580",
+    "url": "/AU_Memo/static/media/illustration3.814bba8b.png"
+  },
+  {
+    "revision": "b988c2cb5b4a0eb9149fdd6e1c82ecbf",
+    "url": "/AU_Memo/static/media/illustration4.b988c2cb.png"
+  },
+  {
+    "revision": "488c9ba05057ad97e5998739f530b452",
+    "url": "/AU_Memo/static/media/illustration5.488c9ba0.png"
+  },
+  {
+    "revision": "61961057783cd279eb612b5c5f8cb85d",
+    "url": "/AU_Memo/static/media/illustration6.61961057.png"
+  },
+  {
+    "revision": "c0dcbf3dd9893b540465747065ae9a4a",
+    "url": "/AU_Memo/static/media/illustration7.c0dcbf3d.png"
+  },
+  {
+    "revision": "7f3422f4cf57879f085248eac47fa1e8",
+    "url": "/AU_Memo/static/media/illustration8.7f3422f4.png"
+  },
+  {
+    "revision": "765a39257945f42c0ab5e0cee56ecaee",
+    "url": "/AU_Memo/static/media/illustration9.765a3925.png"
+  },
+  {
+    "revision": "e1ab48de737ed92dfeb7af56efd25827",
+    "url": "/AU_Memo/static/media/illustration10.e1ab48de.png"
+  },
+  {
+    "revision": "fa7fe84ad1ca01759bd597b9810aee72",
+    "url": "/AU_Memo/static/media/illustration11.fa7fe84a.png"
+  },
+  {
+    "revision": "bae1340d7980f7ceb2cd0a78b9789489",
+    "url": "/AU_Memo/static/media/illustration12.bae1340d.png"
+  },
+  {
+    "revision": "398d2dbe471e65a65b4d509352be14ac",
+    "url": "/AU_Memo/static/media/illustration13.398d2dbe.png"
+  },
+  {
+    "revision": "78909078061a658ba1c9b2ff5d589a47",
+    "url": "/AU_Memo/static/media/illustration14.78909078.png"
+  },
+  {
+    "revision": "be42e87ecb6be7568e3951dc61cc33e9",
+    "url": "/AU_Memo/static/media/illustration15.be42e87e.png"
+  },
+  {
+    "revision": "2fe1cacc2e6db98419dcec612a9c03da",
+    "url": "/AU_Memo/static/media/illustration16.2fe1cacc.png"
+  },
+  {
+    "revision": "7b7ab710c904b22a59f619f5f3042b3b",
+    "url": "/AU_Memo/static/media/illustration17.7b7ab710.png"
+  },
+  {
+    "revision": "071fabfed043db4576adc4148833eea3",
+    "url": "/AU_Memo/static/media/illustration18.071fabfe.png"
+  },
+  {
+    "revision": "afade51e203e5105d791390a87c7393a",
+    "url": "/AU_Memo/static/media/illustration19.afade51e.png"
+  },
+  {
+    "revision": "9416771d47c933acb2472ea691685978",
+    "url": "/AU_Memo/static/media/videoPreview1.9416771d.png"
+  },
+  {
+    "revision": "38c3062b387ef5481dfa94246921b9f6",
+    "url": "/AU_Memo/static/media/mainImage.38c3062b.png"
+  },
+  {
+    "revision": "8b1a813bbb6a7aaf5a64845400636810",
+    "url": "/AU_Memo/static/media/previewImage.8b1a813b.png"
+  },
+  {
+    "revision": "a2e71d8063a53ada988e9ccfd645f24f",
+    "url": "/AU_Memo/static/media/partHeaderImage1.a2e71d80.png"
+  },
+  {
+    "revision": "d0a44c364ca98852d01560514c9c8eb6",
+    "url": "/AU_Memo/static/media/partHeaderImage2.d0a44c36.png"
+  },
+  {
+    "revision": "8dcd98e8147100f9bed090cb50cde730",
+    "url": "/AU_Memo/static/media/partHeaderImage3.8dcd98e8.png"
+  },
+  {
+    "revision": "481fc5e30b796671867174a4e9a2fd27",
+    "url": "/AU_Memo/static/media/partHeaderImage4.481fc5e3.png"
+  },
+  {
+    "revision": "744688bfdb5f71c3d5c57db8287b7dde",
+    "url": "/AU_Memo/static/media/partHeaderImage5.744688bf.png"
+  },
+  {
+    "revision": "e713737aac5f10ead62bb23faf4ff028",
+    "url": "/AU_Memo/static/media/partHeaderImage6.e713737a.png"
+  },
+  {
+    "revision": "476c78d10135b9c3b0889ee5b0732396",
+    "url": "/AU_Memo/static/media/illustration1.476c78d1.png"
+  },
+  {
+    "revision": "ec6fc2b3d75830c11516215eeabdbb05",
+    "url": "/AU_Memo/static/media/illustration2.ec6fc2b3.png"
+  },
+  {
+    "revision": "47fd0e6044e714253e751ecf44939e4d",
+    "url": "/AU_Memo/static/media/illustration3.47fd0e60.png"
+  },
+  {
+    "revision": "110b4412d2849b60622cd7fcbe99d8f0",
+    "url": "/AU_Memo/static/media/illustration4.110b4412.png"
+  },
+  {
+    "revision": "876f1be8e00a1f90a187cace22a378a3",
+    "url": "/AU_Memo/static/media/illustration5.876f1be8.png"
+  },
+  {
+    "revision": "21a2a7e73e812e4a0d93177169b9cefb",
+    "url": "/AU_Memo/static/media/illustration6.21a2a7e7.png"
+  },
+  {
+    "revision": "511153f16a9fc7cbf392f9988b99d8b9",
+    "url": "/AU_Memo/static/media/illustration7.511153f1.png"
+  },
+  {
+    "revision": "45717a58ebb4fa7f0b7bfd5e4e74c7d5",
+    "url": "/AU_Memo/static/media/illustration8.45717a58.png"
+  },
+  {
+    "revision": "8dde08f2a541a60a632eb2f7ab01b4bf",
+    "url": "/AU_Memo/static/media/illustration9.8dde08f2.png"
+  },
+  {
+    "revision": "108c77eab4b4241aea84581eda998dca",
+    "url": "/AU_Memo/static/media/illustration10.108c77ea.png"
+  },
+  {
+    "revision": "ef84effc08b74c4ce18399b6ee891c8f",
+    "url": "/AU_Memo/static/media/illustration11.ef84effc.png"
+  },
+  {
+    "revision": "ec9a57c803a71944664c94e22a9bf600",
+    "url": "/AU_Memo/static/media/illustration12.ec9a57c8.png"
+  },
+  {
+    "revision": "a5089ff33ac2b4e25445378e89edb8c4",
+    "url": "/AU_Memo/static/media/illustration13.a5089ff3.png"
+  },
+  {
+    "revision": "ac3a8812568f0038fea164ecc3591c33",
+    "url": "/AU_Memo/static/media/illustration14.ac3a8812.png"
+  },
+  {
+    "revision": "d370f853d0927c37f3c14e2be4428433",
+    "url": "/AU_Memo/static/media/illustration15.d370f853.png"
+  },
+  {
+    "revision": "a00a9776aa449432c6194b60d08efdfb",
+    "url": "/AU_Memo/static/media/illustration16.a00a9776.png"
+  },
+  {
+    "revision": "1b471444e1addff34b2b350ad0bd1e78",
+    "url": "/AU_Memo/static/media/illustration17.1b471444.png"
+  },
+  {
+    "revision": "95ebb9875ed12f2791f30103cc9bdbec",
+    "url": "/AU_Memo/static/media/illustration18.95ebb987.png"
+  },
+  {
+    "revision": "3270f157fd50debd46d01e9d4051855c",
+    "url": "/AU_Memo/static/media/illustration19.3270f157.png"
+  },
+  {
+    "revision": "9f73cb67401d4546eba86bcbef2b9444",
+    "url": "/AU_Memo/static/media/illustration20.9f73cb67.png"
+  },
+  {
+    "revision": "ce33323d42542b5f2458e4780da1e981",
+    "url": "/AU_Memo/static/media/illustration21.ce33323d.png"
+  },
+  {
+    "revision": "c44fc6e5693bff438b82bba299727900",
+    "url": "/AU_Memo/static/media/illustration22.c44fc6e5.png"
+  },
+  {
+    "revision": "8e19b58f7570d7b46bb416d0ab70ea02",
+    "url": "/AU_Memo/static/media/illustration23.8e19b58f.png"
+  },
+  {
+    "revision": "4f38f752aa1dd90c64ac16af42a37afd",
+    "url": "/AU_Memo/static/media/illustration24.4f38f752.png"
+  },
+  {
+    "revision": "0661ec874ff9ce16033d080c3fbe63da",
+    "url": "/AU_Memo/static/media/illustration25.0661ec87.png"
+  },
+  {
+    "revision": "fea6e3bb55dee7f5c91329af36ed064c",
+    "url": "/AU_Memo/static/media/videoPreview1.fea6e3bb.png"
+  },
+  {
+    "revision": "5c2acb58fc67427056457c48a64bcb35",
+    "url": "/AU_Memo/static/media/videoPreview2.5c2acb58.png"
+  },
+  {
+    "revision": "6df201a43c11e15374941e7771bc05ff",
+    "url": "/AU_Memo/static/media/videoPreview3.6df201a4.png"
+  },
+  {
+    "revision": "618e51997fa0c2a8a3cc30f5fde1adb8",
+    "url": "/AU_Memo/static/media/videoPreview4.618e5199.png"
+  },
+  {
+    "revision": "7a79cb9426268fec8ef421a747c5227d",
+    "url": "/AU_Memo/static/media/sketchImage1.7a79cb94.png"
+  },
+  {
+    "revision": "9c06547a1d83c5be6de5b92b9f108a4c",
+    "url": "/AU_Memo/static/media/ASidayakin.9c06547a.jpg"
+  },
+  {
+    "revision": "3b82dff26c167130ba811c3c710f07fc",
+    "url": "/AU_Memo/static/media/VPutin.3b82dff2.jpg"
+  },
+  {
+    "revision": "df42798ab73e60070ca1b1371e37ca96",
+    "url": "/AU_Memo/static/media/DDubrovskiy.df42798a.jpg"
+  },
+  {
+    "revision": "1306b2b415264dee68dcee61cd54e65f",
+    "url": "/AU_Memo/static/media/previewImage.1306b2b4.png"
+  },
+  {
+    "revision": "ca27befd868ba39c6f39e04dc2631369",
+    "url": "/AU_Memo/static/media/mainImage.ca27befd.png"
+  },
+  {
+    "revision": "b67fb9e247b9b91bc5d25b0fc7ad945c",
+    "url": "/AU_Memo/static/media/partHeaderImage1.b67fb9e2.png"
+  },
+  {
+    "revision": "6e680c20c18f214ed020a65abd0cc09e",
+    "url": "/AU_Memo/static/media/partHeaderImage2.6e680c20.png"
+  },
+  {
+    "revision": "d1320f78ff25480fddbe2c7862692cfd",
+    "url": "/AU_Memo/static/media/partHeaderImage3.d1320f78.png"
+  },
+  {
+    "revision": "6263128135291e462df2a44b151d6fc3",
+    "url": "/AU_Memo/static/media/partHeaderImage4.62631281.png"
+  },
+  {
+    "revision": "2d4b33c5f114103306ff5a7c171c1597",
+    "url": "/AU_Memo/static/media/partHeaderImage5.2d4b33c5.png"
+  },
+  {
+    "revision": "6aeca5baeced389bee55bed9257f6e35",
+    "url": "/AU_Memo/static/media/partHeaderImage6.6aeca5ba.png"
+  },
+  {
+    "revision": "eac96d4649946072d6c1a0962a845cf3",
+    "url": "/AU_Memo/static/media/partHeaderImage7.eac96d46.png"
+  },
+  {
+    "revision": "a1ac3d6a9b2739633ae6100950873f9b",
+    "url": "/AU_Memo/static/media/partHeaderImage8.a1ac3d6a.png"
+  },
+  {
+    "revision": "3caf0e99ee0422ad2402380e65664748",
+    "url": "/AU_Memo/static/media/partHeaderImage9.3caf0e99.png"
+  },
+  {
+    "revision": "7da9f4a327d43110642ffd899519f4f7",
+    "url": "/AU_Memo/static/media/illustration1.7da9f4a3.png"
+  },
+  {
+    "revision": "bf6d08ad63a7ed965081bd5db7348710",
+    "url": "/AU_Memo/static/media/illustration2.bf6d08ad.png"
+  },
+  {
+    "revision": "d68afbcdd3164d90c99b0fadcdb78794",
+    "url": "/AU_Memo/static/media/illustration3.d68afbcd.png"
+  },
+  {
+    "revision": "7ca018db29e637eae05cb562cf0a40f9",
+    "url": "/AU_Memo/static/media/illustration4.7ca018db.png"
+  },
+  {
+    "revision": "632b21e6473a851cdfbfc7a158f79d1c",
+    "url": "/AU_Memo/static/media/illustration5.632b21e6.png"
+  },
+  {
+    "revision": "3793c030c0f30b6aebcb5762796003e0",
+    "url": "/AU_Memo/static/media/illustration6.3793c030.png"
+  },
+  {
+    "revision": "9efd29cded1fa4be8dad46ce40ef4394",
+    "url": "/AU_Memo/static/media/illustration7.9efd29cd.png"
+  },
+  {
+    "revision": "1997d757e0c385c09964050d7408b511",
+    "url": "/AU_Memo/static/media/illustration8.1997d757.png"
+  },
+  {
+    "revision": "a5f2722b5da6c9e5cb97871c2c7e8639",
+    "url": "/AU_Memo/static/media/illustration9.a5f2722b.png"
+  },
+  {
+    "revision": "ebb8d153a962e806793ad8bcbe38a9ff",
+    "url": "/AU_Memo/static/media/illustration10.ebb8d153.png"
+  },
+  {
+    "revision": "ee171453201d05f4d8cc77273fbe8d9c",
+    "url": "/AU_Memo/static/media/illustration11.ee171453.png"
+  },
+  {
+    "revision": "9c9c0f162a4f468f83bd687ce4b267b8",
+    "url": "/AU_Memo/static/media/illustration12.9c9c0f16.png"
+  },
+  {
+    "revision": "1c20b80d7d62562e226feaa737d57332",
+    "url": "/AU_Memo/static/media/illustration13.1c20b80d.png"
+  },
+  {
+    "revision": "d92e57129d9d1e3e1f8b42dd2edc5a69",
+    "url": "/AU_Memo/static/media/illustration14.d92e5712.png"
+  },
+  {
+    "revision": "4b64636103fdbf166df76f8f644f0c0a",
+    "url": "/AU_Memo/static/media/illustration15.4b646361.png"
+  },
+  {
+    "revision": "ce33323d42542b5f2458e4780da1e981",
+    "url": "/AU_Memo/static/media/illustration16.ce33323d.png"
+  },
+  {
+    "revision": "d3cb5bfe1e75dba0f2010cad44d1d907",
+    "url": "/AU_Memo/static/media/illustration17.d3cb5bfe.png"
+  },
+  {
+    "revision": "04964fb75d8bb64a0e36d17f0dcc00b2",
+    "url": "/AU_Memo/static/media/illustration18.04964fb7.png"
+  },
+  {
+    "revision": "33db32d21cc57bf13dace97c4a9e043a",
+    "url": "/AU_Memo/static/media/illustration19.33db32d2.png"
+  },
+  {
+    "revision": "65d6641dc42bf6deb0d95f728f020ac3",
+    "url": "/AU_Memo/static/media/illustration20.65d6641d.png"
+  },
+  {
+    "revision": "783b74c2301231a109483d8cc42f56a1",
+    "url": "/AU_Memo/static/media/illustration21.783b74c2.png"
+  },
+  {
+    "revision": "8109c26a92e8415abd952224f47c8072",
+    "url": "/AU_Memo/static/media/illustration22.8109c26a.png"
+  },
+  {
+    "revision": "fc6395e428d98ac6cbfdf961a9b7cb2e",
+    "url": "/AU_Memo/static/media/illustration23.fc6395e4.png"
+  },
+  {
+    "revision": "c3adf875df0d98b5bfa861730ee26495",
+    "url": "/AU_Memo/static/media/illustration25.c3adf875.png"
+  },
+  {
+    "revision": "f5fe63b6793e7f9bb026f5a25fa96b6f",
+    "url": "/AU_Memo/static/media/illustration26.f5fe63b6.png"
+  },
+  {
+    "revision": "07cd81676876ad92386a45865dc22177",
+    "url": "/AU_Memo/static/media/sketchImage1.07cd8167.png"
+  },
+  {
+    "revision": "353b60bcbb6e59d2d4caf263b312c1b7",
+    "url": "/AU_Memo/static/media/sketchImage2.353b60bc.png"
+  },
+  {
+    "revision": "a47afdd92be3e710bf278c8c9195b931",
+    "url": "/AU_Memo/static/media/sketchImage3.a47afdd9.png"
+  },
+  {
+    "revision": "f21ea5aa5b012f0a1413e53e789a2ee6",
+    "url": "/AU_Memo/static/media/firstSeriesHeader.f21ea5aa.jpg"
+  },
+  {
+    "revision": "19cca6ffeec83082652dcfae07dfc6ac",
+    "url": "/AU_Memo/static/media/thirdPartImage.19cca6ff.jpg"
+  },
+  {
+    "revision": "2212f5b1f8c416483a97b1cdf8cd1481",
+    "url": "/AU_Memo/static/media/illustration1.2212f5b1.jpg"
+  },
+  {
+    "revision": "18cbb1061886634abe4ca95b81c18355",
+    "url": "/AU_Memo/static/media/illustration2.18cbb106.jpg"
+  },
+  {
+    "revision": "6e37b38f86f02d3a4d37a46614528dfe",
+    "url": "/AU_Memo/static/media/partHeader1.6e37b38f.jpg"
+  },
+  {
+    "revision": "73a0462dbc799ed64a94ef57b3355f1f",
+    "url": "/AU_Memo/static/media/partHeader2.73a0462d.jpg"
+  },
+  {
+    "revision": "c6a70e9a369aa78aa95c4f7f59d247b3",
+    "url": "/AU_Memo/static/media/sketchImage1.c6a70e9a.jpg"
+  },
+  {
+    "revision": "4eced519c23c72f65b3ebc3c0c0276d2",
+    "url": "/AU_Memo/static/media/seriesImage.4eced519.jpg"
+  },
+  {
+    "revision": "62be05a03e85c71f187cf4b8c6e602f7",
+    "url": "/AU_Memo/static/media/previewImage.62be05a0.png"
+  },
+  {
+    "revision": "36e6cb1b6552f26cae08ed9bcdf0744a",
+    "url": "/AU_Memo/static/media/timelineHeaderImage.36e6cb1b.png"
+  },
+  {
+    "revision": "b5973a5b4f366f9bb9617d447025ffb9",
+    "url": "/AU_Memo/static/media/mainTitleImage.b5973a5b.png"
+  },
+  {
+    "revision": "e5fa4736cf31046e6556e642ebb65597",
+    "url": "/AU_Memo/static/media/quant-antiqua-2.e5fa4736.woff"
+  },
+  {
+    "revision": "17bbc4e84cf9c025035bbfa814a1213d",
+    "url": "/AU_Memo/static/media/quant-antiqua-italic-001-001.17bbc4e8.woff"
+  },
+  {
+    "revision": "a5648485024e834a313363c9bab50883",
+    "url": "/AU_Memo/static/media/ofont.ru_Garamond-Narrow.a5648485.woff"
+  },
+  {
+    "revision": "9807f8be30adcbc0cb2bc72d14bbfc21",
+    "url": "/AU_Memo/static/media/RF-Rostin-Regular.9807f8be.woff"
+  },
+  {
+    "revision": "8706912d10e7e3ccc0c1623e9a231891",
+    "url": "/AU_Memo/static/media/Lithium_tf.8706912d.woff"
+  },
+  {
+    "revision": "c280e852a8943210ee5c5f709bcc40a2",
+    "url": "/AU_Memo/static/media/cross.c280e852.svg"
+  },
+  {
+    "revision": "60fc9198044d71234de2f2dec3669dcb",
+    "url": "/AU_Memo/static/media/Vector.60fc9198.svg"
+  },
+  {
+    "revision": "ecc74c7c3cec188ebf503176319c0f1f",
+    "url": "/AU_Memo/static/media/quotes.ecc74c7c.svg"
+  },
+  {
+    "revision": "aa28128a7df0fb0c9768f7468e9b9c76",
+    "url": "/AU_Memo/index.html"
+  }
+];
